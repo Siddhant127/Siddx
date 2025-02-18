bd8173fbfa05219aa2224a0ff309e8511029bbbe
Siddhant127 <siddhantchetry128@gmail.com>
2025-02-18 12:42:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Sidd...',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Sidd Bot",
+  botOwner: 'Sidd..', // Your name
+  ownerNumber: '918135813207', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
